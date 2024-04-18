86feb1bf107bca178838071165eee90e7676ec2a
Gurdeep Singh <grdp99@gmail.com>
2024-04-18 22:11:57 +0530

    Whatsapp Server Node By Gurdeep

---
+++ config.js
@@ -0,0 +1,17 @@
+var config = {
+	debug: false,
+	database: {
+		host: "localhost",
+		user: "intechost",
+		password: "intechost",
+		database: "intechost",
+		charset : "utf8mb4"
+	},
+	dev: "webflop.net",
+	cors: {
+		origin: '*',
+ 		optionsSuccessStatus: 200
+	}
+}
+
+module.exports = config; 
